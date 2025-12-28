5228d94fe86619caadae70e30689771eeccf41cc
Javian Ng <javian.ng.z.h@gmail.com>
2025-12-28 15:44:40 +0800

    commit

--- src/app/photoset/[id]/PhotosetPageClient.tsx
+++ src/app/photoset/[id]/PhotosetPageClient.tsx
@@ -32,22 +32,22 @@ export function PhotosetPageClient({
         <section className="flex w-full flex-col gap-10 py-6">
           <Button className="w-fit font-thin" variant="link" size="none">
             <Link href={"/"}>
-              <small>Back to Home</small>
+              <small>&lsaquo; Back to Home</small>
             </Link>
           </Button>
           <h1 className="text-xl font-thin">{photosetTitle}</h1>
         </section>
 
-        <hr className="mb-10 h-[1px] w-full bg-neutral-200" />
+        <hr className="mb-10 h-px w-full bg-neutral-200" />
 
-        <section className="grid w-full grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <section className="grid w-full grid-cols-1 gap-6 sm:grid-cols-2">
           {photos.map((photo) => (
             <div key={photo.id} className="relative">
               <Image
                 src={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_c.jpg`}
                 alt={`${photo.title || "Photograph"} - ${photosetTitle} collection by Javian Ng`}
-                width={300}
-                height={300}
+                width={1600}
+                height={1600}
                 className="h-full w-full object-cover"
               />
             </div>
